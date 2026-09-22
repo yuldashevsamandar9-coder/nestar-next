13476d5f67c9edc0456dd33748fea4ee963fce01
Sam <yuldashevsamandar9@gmail.com>
2026-09-22 20:43:43 +0900

    feat: develop Like screen component

--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -10,11 +10,12 @@ import { PropertiesInquiry } from '../../libs/types/property/property.input';
 import { Property } from '../../libs/types/property/property';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded';
-import { Direction } from '../../libs/enums/common.enum';
+import { Direction, Message } from '../../libs/enums/common.enum';
 import { useMutation, useQuery } from '@apollo/client';
 import { LIKE_TARGET_PROPERTY } from '../../apollo/user/mutation';
 import { GET_PROPERTIES } from '../../apollo/user/query';
 import { T } from '../../libs/types/common';
+import { sweetMixinErrorAlert, sweetTopSmallSuccessAlert } from '../../libs/sweetAlert';
 
 export const getStaticProps = async ({ locale }: any) => ({
 	props: {
@@ -66,6 +67,7 @@ const PropertyList: NextPage = ({ initialInput, ...props }: any) => {
 	useEffect(() => {}, [searchFilter]);
 
 	/** HANDLERS **/
+
 	const handlePaginationChange = async (event: ChangeEvent<unknown>, value: number) => {
 		searchFilter.page = value;
 		await router.push(
@@ -86,6 +88,20 @@ const PropertyList: NextPage = ({ initialInput, ...props }: any) => {
 	const sortingCloseHandler = () => {
 		setSortingOpen(false);
 		setAnchorEl(null);
+	};
+
+	const likePropertyHandler = async (user: T, id: string) => {
+		try {
+			if (!id) return;
+			if (!user._id) throw new Error(Message.NOT_AUTHENTICATED);
+			// execute likeTargetProperty Mutation
+			await likeTargetProperty({ variables: { input: id } });
+			await getPropertiesRefetch({ input: initialInput });
+			await sweetTopSmallSuccessAlert('success', 800);
+		} catch (err: any) {
+			console.log('ERROR, likePropertyHandler:', err.message);
+			sweetMixinErrorAlert(err.message).then();
+		}
 	};
 
 	const sortingHandler = (e: React.MouseEvent<HTMLLIElement>) => {
@@ -160,7 +176,9 @@ const PropertyList: NextPage = ({ initialInput, ...props }: any) => {
 									</div>
 								) : (
 									properties.map((property: Property) => {
-										return <PropertyCard property={property} key={property?._id} />;
+										return (
+											<PropertyCard property={property} likePropertyHandler={likePropertyHandler} key={property?._id} />
+										);
 									})
 								)}
 							</Stack>
